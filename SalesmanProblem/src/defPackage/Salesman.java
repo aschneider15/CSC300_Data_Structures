61b837d99bc663234412078c5f5f1de28b256414
aschneider15 <aschneider15@outlook.com>
2022-09-09 13:08:47 -0500

    Salesman Problem - Final Revisions
    Moved the bubble sort function from the Driver class to the Salesman class (made more sense to me).
    
    Changed the bubble sort algorithm to sort the array in descending order rather than ascending order

--- SalesmanProblem/src/defPackage/Salesman.java
+++ SalesmanProblem/src/defPackage/Salesman.java
@@ -22,5 +22,33 @@ public class Salesman {
 	public void setSales(int salesTotal) {
 		this.salesTotal = salesTotal;
 	}
+	
+	public static Salesman[] topFive(Salesman[] in) {
+		/*int temp = 0;
+		 * for(length of array -1)
+		 * 	look at element i and element i+1
+		 * 	if i > i+1
+		 * 		swap i and i+1
+		 * 	increment i
+		 * 
+		 * repeat this process for the length of the array
+		 */
+		Salesman[] topFive = new Salesman[5];
+		Salesman temp;
+		
+		for(int j = in.length; j > 0; j--) {
+			for(int i = 0; i < in.length - 1; i++) {
+				if(in[i].getSales() < in[i+1].getSales()) {
+					temp = in[i];
+					in[i] = in[i+1];
+					in[i+1] = temp;
+				}
+			}
+		}
+		for(int i = 0; i < 5; i++) {
+			topFive[i] = in[i];
+		}
+		return topFive;
+	}
 
 }
