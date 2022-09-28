c8c51852a4e29f4a8fe435e709d8e11deae68030
aschneider15 <aschneider15@outlook.com>
2022-09-28 13:29:19 -0500

    Completed Great Commission Assignment

---
+++ The Great Commission/src/DisciplePkg/Driver.java
@@ -0,0 +1,32 @@
+package DisciplePkg;
+
+public class Driver {
+	public static void main(String[] args) {
+		long numDisciples = 13;
+		long totalPopulation = 7700;
+		totalPopulation *= 1000000;
+		
+		System.out.println(howManyYears(numDisciples, totalPopulation));
+		System.out.println(howManyYears50(numDisciples, totalPopulation));
+
+		
+	}
+	
+	public static int howManyYears(long initialDisciples, long totalPop) {
+		int years = 0;
+		while(initialDisciples < totalPop) {
+			initialDisciples *=2;
+			years += 3;
+		}
+		return years;
+	}
+	
+	public static int howManyYears50(long initialDisciples, long totalPop) {
+		int years = 0;
+		while(initialDisciples < totalPop) {
+			initialDisciples *=4;
+			years += 3;
+		}
+		return years;
+	}
+}
