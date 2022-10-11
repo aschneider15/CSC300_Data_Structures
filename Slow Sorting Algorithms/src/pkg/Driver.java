51bd6cb06f53e6630e79d2848c590c41f95e5fe8
aschneider15 <aschneider15@outlook.com>
2022-10-11 10:17:49 -0500

    finished final touches on the Slow Sorts assignment (mostly cosmetic changes)

--- Slow Sorting Algorithms/src/pkg/Driver.java
+++ Slow Sorting Algorithms/src/pkg/Driver.java
@@ -3,30 +3,32 @@ package pkg;
 public class Driver {
 
 	public static void main(String[] args) {
-		int[] bubble = new int[] {8, 3, 9, 5, 1, 7, 2, 6, 0, 4};
-		int[] select = new int[] {4, 2, 7, 5, 3, 9, 6, 0, 8, 1};
-		int[] insert = new int[] {6, 1, 9, 0, 7, 2, 3, 7, 4, 5};
-		int[] shella = new int[] {9, 5, 1, 3, 5, 7, 22, 4, 18, 7, 14, 20, 0, 38, 2, 17};
+		int[] list0 = new int[] {9, 5, 1, 3, 5, 7, 22, 4, 18, 7, 14, 20, 0, 38, 2, 17};
+		int[] list1 = new int[] {9, 5, 1, 3, 5, 7, 22, 4, 18, 7, 14, 20, 0, 38, 2, 17};
+		int[] list2 = new int[] {9, 5, 1, 3, 5, 7, 22, 4, 18, 7, 14, 20, 0, 38, 2, 17};
+		int[] list3 = new int[] {9, 5, 1, 3, 5, 7, 22, 4, 18, 7, 14, 20, 0, 38, 2, 17};
 		
-		printArray(bubble);
-		sort_bubble(bubble);
-		printArray(bubble);
-		
-		System.out.println();
-		
-		printArray(select);
-		sort_selection(select);
-		printArray(select);
+		printArray(list0);
+		sort_bubble(list0);
+		printArray(list0);
 		
 		System.out.println();
 		
-		printArray(insert);
-		sort_insertion(insert);
-		printArray(insert);
+		printArray(list1);
+		sort_selection(list1);
+		printArray(list1);
 		
-		printArray(shella);
-		sort_shell(shella);
-		printArray(shella);
+		System.out.println();
+		
+		printArray(list2);
+		sort_insertion(list2);
+		printArray(list2);
+		
+		System.out.println();
+		
+		printArray(list3);
+		sort_shell(list3);
+		printArray(list3);
 	}
 	
 	public static void printArray(int[] arr) {
@@ -45,62 +47,51 @@ public class Driver {
 	}
 	//Bubble Sort algorithm
 	public static void sort_bubble(int[] arr) {
-		int comparisonCount = 0;//counts the number of comparisons made during sorting
 		int swapCount = 0;//counts the number of swaps during algorithm
 		for(int i = 1; i < arr.length-1; i++) {
 			for(int j = 0; j < arr.length -i; j++) {
-				comparisonCount++;
 				if(arr[j] > arr[j+1]) {//if first element is greater than second,
 					swapElements(arr, j, j+1);//swap them
 					swapCount++;
 				}
 			}
 		}
-		System.out.println("Finished! Number of Comparisons: " + comparisonCount 
-											+ "\tNumber of swaps: " + swapCount);
+		System.out.println("Finished! Number of swaps: " + swapCount);
 	}
 	
 	//Selection Sort algorithm
 	public static void sort_selection(int[] arr) {
-		int comparisonCount = 0;
 		int swapCount = 0;
 		int smallestIndex = 0;//used to hold the index of smallest value
 		for(int i = 0; i < arr.length-1; i++) {
 			for(int j = i+1; j < arr.length; j++) {
-				comparisonCount++;
 				if(arr[j] < arr[smallestIndex])//if smaller than the current smallest index,
 					smallestIndex = j;//then replace smallest index with current
 			}
 			swapElements(arr, i, smallestIndex);//always swaps the current index "i" with smallest number
 			swapCount++;//this process begins at the front-end of the array
 		}
-		System.out.println("Finished! Number of Comparisons: " + comparisonCount 
-				+ "\tNumber of swaps: " + swapCount);	
+		System.out.println("Finished! Number of swaps: " + swapCount);
 	}
 	
 	public static void sort_insertion(int[] arr) {
-		int comparisonCount = 0;
 		int swapCount = 0;
 		for(int i = 1; i < arr.length; i++) {
 			int j = i-1;
 			while(j > 0 && arr[j] < arr[j-1]) {
-				comparisonCount++;
 				swapElements(arr, j, j-1);
 				swapCount++;
 				j--;
 			}
 		}		
-		System.out.println("Finished! Number of Comparisons: " + comparisonCount 
-				+ "\tNumber of swaps: " + swapCount);
+		System.out.println("Finished! Number of swaps: " + swapCount);
 	}
 	
 	public static void sort_shell(int[] arr) {
-		int comparisonCount = 0;
 		int swapCount = 0;
-		for(int gap = (int)(Math.log(arr.length) / Math.log(2)) -1; gap > 0; gap--) {
+		for(int gap = (int)(Math.sqrt(arr.length)) -1; gap > 0; gap--) {
 			for(int i = gap; i < arr.length; i++) {
 				int j = i;
-				comparisonCount++;
 				while(j >= gap && arr[j] < arr[j-gap]) {
 					swapElements(arr, j, j-gap);
 					swapCount++;
@@ -108,7 +99,6 @@ public class Driver {
 				}
 			}
 		}
-		System.out.println("Finished! Number of Comparisons: " + comparisonCount 
-				+ "\tNumber of swaps: " + swapCount);
+		System.out.println("Finished! Number of swaps: " + swapCount);
 	}
 }
